ccb149bde353ffae4d393a2cec5535d38d988bf4
Dipanshu <dipanshusingh588@gmail.com>
2023-03-15 15:46:00 +0530

    Started OpenCV code tutorials. Added basic_opencv.py and Lena.png

---
+++ basic_opencv.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+# print(cv.__version__)       # to check the version of opencv
+
+# to read an image
+img = cv.imread('Lena.png', -1)
+print(img)          # it will print the matrix of pixels read by opencv
+
+# to display an image
+cv.imshow('image', img)         
+
+# cv.waitKey(5000)                # wait for 5 sec
+cv.waitKey(0)                   # wait forever
+
+# destroys all windows created by opencv
+cv.destroyAllWindows()
+
+# to write to a file using opencv
+cv.imwrite('lena_copy.jpg', img)
