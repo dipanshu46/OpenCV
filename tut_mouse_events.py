f8aa68d6bd1bb85894f1d5edb5eabe39c56d9807
Dipanshu <dipanshusingh588@gmail.com>
2023-03-18 14:48:31 +0530

    Updated tut_mouse_events.py

--- tut_mouse_events.py
+++ tut_mouse_events.py
@@ -1,15 +1,17 @@
+'''
+Mouse events can be handled using cv2 library.
+
+List of available flags for mouse events :-
+
+['EVENT_FLAG_ALTKEY', 'EVENT_FLAG_CTRLKEY', 'EVENT_FLAG_LBUTTON', 'EVENT_FLAG_MBUTTON', 'EVENT_FLAG_RBUTTON', 'EVENT_FLAG_SHIFTKEY', 'EVENT_LBUTTONDBLCLK', 'EVENT_LBUTTONDOWN', 'EVENT_LBUTTONUP', 'EVENT_MBUTTONDBLCLK', 'EVENT_MBUTTONDOWN', 'EVENT_MBUTTONUP', 'EVENT_MOUSEHWHEEL', 'EVENT_MOUSEMOVE', 'EVENT_MOUSEWHEEL', 'EVENT_RBUTTONDBLCLK', 'EVENT_RBUTTONDOWN', 'EVENT_RBUTTONUP']
+
+Flags can be searched using the below 2 lines of code after importing cv2.
+events = [i for i in dir(cv2) if 'EVENT' in i]
+print(events)
+'''
+
 import numpy as np
 import cv2
-
-# Mouse events can be handled using cv2 library.
-
-# List of available flags for mouse events :-
-
-# ['EVENT_FLAG_ALTKEY', 'EVENT_FLAG_CTRLKEY', 'EVENT_FLAG_LBUTTON', 'EVENT_FLAG_MBUTTON', 'EVENT_FLAG_RBUTTON', 'EVENT_FLAG_SHIFTKEY', 'EVENT_LBUTTONDBLCLK', 'EVENT_LBUTTONDOWN', 'EVENT_LBUTTONUP', 'EVENT_MBUTTONDBLCLK', 'EVENT_MBUTTONDOWN', 'EVENT_MBUTTONUP', 'EVENT_MOUSEHWHEEL', 'EVENT_MOUSEMOVE', 'EVENT_MOUSEWHEEL', 'EVENT_RBUTTONDBLCLK', 'EVENT_RBUTTONDOWN', 'EVENT_RBUTTONUP']
-
-# Flags can be searched using the below 2 lines of code after importing cv2.
-# events = [i for i in dir(cv2) if 'EVENT' in i]
-# print(events)
 
 # update image function
 def updateScreen(s,x,y,color):
@@ -47,6 +49,7 @@ def click_event(event, x,y, flags, param):
 img = cv2.imread('data/leuvenA.jpg', 1)
 cv2.imshow('image', img)
 
+# to catch mouse event and send it to our mouse event handler use this function
 cv2.setMouseCallback('image', click_event)
 cv2.waitKey(0)
 
